fd4d01a7c8c972fdd90e343779dc8d64a1938c5c
PetitePluie <wpy031025@163.com>
2025-11-26 14:36:32 +0800

    perf(interceptors): improved type safety

--- src/interceptors.ts
+++ src/interceptors.ts
@@ -3,20 +3,24 @@
  * 链式 add/remove、异步执行
  */
 
-import type { RequestInterceptor, ResponseInterceptor } from './types';
+import type {
+  RequestInterceptor,
+  RequestOptions,
+  ResponseInterceptor,
+} from './types';
 
-export class InterceptorManager<T> {
+export class InterceptorManager<V, F = (_value: V) => V | Promise<V>> {
   private interceptors: Array<{
     id: number;
-    fulfilled?: T;
-    rejected?: (_error: unknown) => unknown;
+    fulfilled?: F;
+    rejected?: (_error: unknown) => V | Promise<V>;
   }> = [];
   private nextId = 0;
 
   /**
    * 添加拦截器
    */
-  use(fulfilled?: T, rejected?: (_error: unknown) => unknown): number {
+  use(fulfilled?: F, rejected?: (_error: unknown) => V | Promise<V>): number {
     const id = this.nextId++;
     this.interceptors.push({ id, fulfilled, rejected });
     return id;
@@ -47,8 +51,8 @@ export class InterceptorManager<T> {
   forEach(
     fn: (_interceptor: {
       id: number;
-      fulfilled?: T;
-      rejected?: (_error: unknown) => unknown;
+      fulfilled?: F;
+      rejected?: (_error: unknown) => V | Promise<V>;
     }) => void
   ): void {
     this.interceptors.forEach(fn);
@@ -62,30 +66,52 @@ export class InterceptorManager<T> {
   }
 }
 
-export class RequestInterceptorManager extends InterceptorManager<RequestInterceptor> {
+export class RequestInterceptorManager extends InterceptorManager<
+  RequestOptions,
+  RequestInterceptor
+> {
   /**
    * 执行请求拦截器链
    */
-  async run(config: unknown): Promise<unknown> {
-    let promise = Promise.resolve(config);
+  async run(config: RequestOptions): Promise<RequestOptions> {
+    let promise: Promise<RequestOptions> = Promise.resolve(config);
 
     this.forEach(({ fulfilled, rejected }) => {
-      promise = promise.then(fulfilled, rejected);
+      promise = promise.then(
+        fulfilled
+          ? (value: RequestOptions): RequestOptions | Promise<RequestOptions> =>
+              fulfilled(value)
+          : (value: RequestOptions) => value,
+        rejected
+          ? (error: unknown): RequestOptions | Promise<RequestOptions> =>
+              rejected(error)
+          : undefined
+      );
     });
 
     return promise;
   }
 }
 
-export class ResponseInterceptorManager extends InterceptorManager<ResponseInterceptor> {
+export class ResponseInterceptorManager extends InterceptorManager<
+  Response,
+  ResponseInterceptor
+> {
   /**
    * 执行响应拦截器链
    */
   async run(response: Response): Promise<Response> {
-    let promise = Promise.resolve(response);
+    let promise: Promise<Response> = Promise.resolve(response);
 
     this.forEach(({ fulfilled, rejected }) => {
-      promise = promise.then(fulfilled, rejected);
+      promise = promise.then(
+        fulfilled
+          ? (value: Response): Response | Promise<Response> => fulfilled(value)
+          : (value: Response) => value,
+        rejected
+          ? (error: unknown): Response | Promise<Response> => rejected(error)
+          : undefined
+      );
     });
 
     return promise;
